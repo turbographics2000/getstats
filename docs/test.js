34d499f01444b6773aa14bb395b19d229f00e35a
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-13 17:03:41 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -156,10 +156,10 @@ function pcSetup(remoteId) {
 }
 
 function getStats(side) {
-  console.log(side);
   var view = window[side + 'View'];
   var track = view.srcObject.getVideoTracks()[0];
   var container = window[side + 'StreamStatsContainer'];
+  console.log(side, track.id);
   pc.getStats(track).then(report => {
     report.forEach(now => {
       var reportMemberDiv = window['rpt' + side + now.id];
