060732d2e579487f163ec7920d13a6e7d9f8572f
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-13 15:32:33 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -19,7 +19,7 @@ function getRmoteIds() {
   var getRemoteIdSIId = null;
   getRemoteIdSIId = setInterval(_ => {
     fetch(`https://skyway.io/active/list/${apiKey}`).then(res => res.json()).then(list => {
-      var remotIds = list.filter(memberId => memberId !== id);
+      var remotIds = list.filter(memberId => memberId !== myId);
       if (remotIds.length) {
         callTo.value = remoteIds[0];
         clearInterval(getRemoteIdSIId);
